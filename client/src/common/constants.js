7a4a51e3abbda5948ef4f4284ce4cab8a59cd624
JordanDim <dankooo@gmail.com>
2024-05-01 15:50:18 +0300

    initial constants file

---
+++ client/src/common/constants.js
@@ -0,0 +1,8 @@
+export const USER_NAME_MIN_LENGTH = 4;
+export const USER_NAME_MAX_LENGTH = 32;
+export const ADMIN_NAME_MIN_LENGTH = 4;
+export const ADMIN_NAME_MAX_LENGTH = 32;
+export const POST_TITLE_MIN_LENGTH = 16;
+export const POST_TITLE_MAX_LENGTH = 64;
+export const POST_CONTENT_MIN_LENGTH = 32;
+export const POST_CONTENT_MAX_LENGTH = 8192;

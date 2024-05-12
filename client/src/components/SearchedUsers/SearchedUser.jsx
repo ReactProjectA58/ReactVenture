06ae2c0a43048f5d738000ead051efa3e11e503e
JordanDim <dankooo@gmail.com>
2024-05-12 22:26:01 +0300

    repair SearchedUser

--- client/src/components/SearchedUsers/SearchedUser.jsx
+++ client/src/components/SearchedUsers/SearchedUser.jsx
@@ -1,11 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 import "./SearchedUser.css";
 import { AppContext } from "../../context/AppContext";
 import { Link } from "react-router-dom";
+import { blockUser, unblockUser } from "../../services/users.service";
 
 export default function SearchedUser({ user }) {
   const { userData } = useContext(AppContext);
+  const [isBlocked, setIsBlocked] = useState(user.isBlocked);
   const isAdmin = userData && userData.isAdmin;
+
+  const handleBlockUser = async () => {
+    try {
+      await blockUser(user.handle);
+      setIsBlocked(true);
+    } catch (error) {
+      console.error("Error blocking user:", error);
+    }
+  };
+
+  const handleUnblockUser = async () => {
+    try {
+      await unblockUser(user.handle);
+      setIsBlocked(false);
+    } catch (error) {
+      console.error("Error unblocking user:", error);
+    }
+  };
 
   return (
     <div className="outer-border">
@@ -24,17 +45,36 @@ export default function SearchedUser({ user }) {
           <p className="email-label">Email:</p>
           <div className="email">{user.email}</div>
         </div>
-        {isAdmin ? (
-          <>
-            <button className="block-button">Block user</button>
-            <Link to={`/user/${user.id}`} className="profile-link">
-              <button className="profile-button">Profile</button>
-            </Link>
-          </>
-        ) : (
-          <p className="not-admin-message">You are not authorized to perform this action.</p>
+
+        {isBlocked && (
+          <button className="unblock-button" onClick={handleUnblockUser}>
+            Unblock User
+          </button>
+        )}
+
+        {!isBlocked && (
+          <button className="block-button" onClick={handleBlockUser}>
+            Block User
+          </button>
+        )}
+
+        {isAdmin && (
+          <Link to={`/user/${user.id}`} className="profile-link">
+            <button className="profile-button">Profile</button>
+          </Link>
         )}
       </div>
     </div>
   );
 }
+
+SearchedUser.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    handle: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    isBlocked: PropTypes.bool.isRequired
+  }).isRequired,
+};
